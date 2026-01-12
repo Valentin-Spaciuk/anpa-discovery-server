3a9fb85321afba7998db03df12b53b8d017ac784
Spaciuk, Valentin EXT <valentin.spaciuk@comafi.com.ar>
2026-01-12 16:07:07 -0300

    first commit

---
+++ anpa-discovery-server/src/main/java/com/example/anpa_discovery_server/AnpaDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.anpa_discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnpaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnpaDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ anpa-discovery-server/src/test/java/com/example/anpa_discovery_server/AnpaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anpa_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnpaDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
